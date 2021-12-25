2bab072758252a986683b29d4562cde31580c541
jkajura <jkajura@innovationvillage.co.ug>
2021-12-25 14:09:37 +0300

    Working on components and child and parent referencing

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+ import '../App.css';
+ import Bikes from './bikes';
+ import Cars from './cars';
+
+
+function App() {
+  return (
+    <div>
+      <Bikes />
+      <Cars />
+    </div>
+  );
+}
+
+export default App;
