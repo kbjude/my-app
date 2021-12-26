f5c15edc0197bb8fbacf125e0a342cd4d514fc2b
jkajura <jkajura@innovationvillage.co.ug>
2021-12-26 18:32:11 +0300

    WIP styles

--- src/components/Current.js
+++ src/components/Current.js
@@ -2,22 +2,25 @@ import React from 'react';
 import reactDom from 'react-dom';
 
 function Current() {
+  const styles = {
+    color: "orange",
+    backgroundColor: "grey"
+  }
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay 
   
     if (hours < 12) {
       this.timeOfDay = "Morning"
+      styles.color = "blue"
     }else if (hours >= 12 && hours < 17) {
       timeOfDay = "afternoon"
+      styles.color = "blue"
     }else {
       timeOfDay = "Night"
+      styles.color = "purple"
     }
 
-    const styles = {
-      color: "orange",
-      backgroundColor: "grey"
-    }
     return (
       <h1 style={styles}> Good {timeOfDay}!</h1>
     )
