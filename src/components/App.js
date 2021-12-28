cb30e1ab59390737fb378f688e2dab5724c060e4
jkajura <jkajura@innovationvillage.co.ug>
2021-12-29 00:19:06 +0300

    WIP displaying content from array

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@
  import Current from './Current';
  import ContactCard from './contactcard';
  import cardsData from './cardsData';
+ import Students from './students';
 
 function App() {
   const result = cardsData.map(card => <ContactCard
@@ -12,11 +13,26 @@ function App() {
     phone={card.phone}
     email={card.email}
     />)
-    console.log(result);
 
+    const study = Students.map(student => <ContactCard 
+      key={student.id}
+      name={student.name}
+      grade={student.grade}
+      address={student.address}
+      notes={student.notes}
+      status={student.status}
+    />)
+    console.log(study)
   return (
     <div>
-      {result}
+      <div className="topMenu">
+        <div>
+          {result}
+        </div>
+        <div>
+          {study}
+        </div>
+      </div>
       <Current />
       <Bikes />
       <Cars />
--- src/components/contactcard.js
+++ src/components/contactcard.js
@@ -3,9 +3,15 @@ import React from "react";
 const CardContainer = (props) => {
     return (
         <div className="contact-card">
-            <h3 style={{display: props.name ? "block" : "none"}}>Name: {props.name}</h3>
-            <p style={{color: !props.name && "red"}}>Phone: {props.phone}</p>
-            <p>Email: {props.email}</p>
+            <div>
+                <h3 style={{display: props.name ? "block" : "none"}}>Name: {props.name}</h3>
+                <p style={{color: !props.name && "red"}}>Phone: {props.phone}</p>
+                <p>Email: {props.email}</p>
+            </div>
+            <div>
+                <h2> Name: {props.name}</h2>
+                
+            </div>
         </div> 
     )
 }
