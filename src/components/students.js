290d6e7c1581c8d729e4bd1329bc7479f744f626
jkajura <jkajura@innovationvillage.co.ug>
2021-12-28 17:11:47 +0300

    WIP map resources in react

---
+++ src/components/students.js
@@ -0,0 +1,27 @@
+const students = [
+    {
+        name: "Jude Kajura",
+        grade: '100%',
+        address: "Bunga hill",
+        notes: "This Student has performed realy well",
+        status: "Promoted"
+    },
+
+    {
+        name: "Isaiah Kisembo",
+        grade: '100%',
+        address: "Temangalo",
+        notes: "This Student has performed realy well",
+        status: "Promoted"
+    },
+
+    {
+        name: "Ngonzi Kajura",
+        grade: '100%',
+        address: "Temangalo",
+        notes: "This Student has performed realy well",
+        status: "Promoted"
+    }
+]
+
+export default students
