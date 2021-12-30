3aaefebf954d58b802f680d5af3a7ad4871d94ff
jkajura <jkajura@innovationvillage.co.ug>
2021-12-30 12:13:16 +0300

    WIP component didmount

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
  import '../App.css';
+ import React, {Component} from 'react';
  import Bikes from './bikes';
  import Cars from './cars';
  import Current from './Current';
@@ -6,38 +7,56 @@
  import cardsData from './cardsData';
  import Students from './students';
 
-function App() {
-  const result = cardsData.map(card => <ContactCard
-    key={card.id}
-    name={card.name}
-    phone={card.phone}
-    email={card.email}
-    />)
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      todos: cardsData,
+      isLoading: true
+    }
+  } 
 
-    const study = Students.map(student => <ContactCard 
-      key={student.id}
-      name={student.name}
-      grade={student.grade}
-      address={student.address}
-      notes={student.notes}
-      status={student.status}
-    />)
-    console.log(study)
-  return (
-    <div>
-      <div className="topMenu">
-        {/* <div>
-          {result}
-        </div> */}
-        <div>
-          {study}
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      })
+    }, 1500)
+  }
+
+  render(){
+    const result = this.todos.map(card => <ContactCard
+      key={card.id}
+      name={card.name}
+      phone={card.phone}
+      email={card.email}
+      />)
+
+      const study = Students.map(student => <ContactCard 
+        key={student.id}
+        name={student.name}
+        grade={student.grade}
+        address={student.address}
+        notes={student.notes}
+        status={student.status}
+      />)
+      console.log(study)
+    return (
+      <div>
+        <div className="topMenu">
+          {/* <div>
+            {result}
+          </div> */}
+          <div>
+            {study}
+          </div>
         </div>
+        <Current />
+        <Bikes />
+        <Cars />
       </div>
-      <Current />
-      <Bikes />
-      <Cars />
-    </div>
-  );
+    );
+      }
 }
 
 export default App;
